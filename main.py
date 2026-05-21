16c5b132e2c6d1278f365d2c7a56a364789a7e01
maftunaozodboyeva86@gmail.com <maftunaozodboyeva86>
2026-05-21 17:44:43 +0500

    Add initial bot implementation with dotenv support and requirements

---
+++ main.py
@@ -0,0 +1,19 @@
+import telebot
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Assalom alaykum, portfolio botimizga hush kelibsiz!")
+	
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+	
+bot.infinity_polling()
